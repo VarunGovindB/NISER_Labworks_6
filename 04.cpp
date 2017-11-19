c72b925f1411ced3f0f673659dae2f0ee0fb7c67
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-11-19 10:56:56 +0530

    Update 04.cpp

--- 04.cpp
+++ 04.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	char arr[10]={'d','a','n','u','s','h'},*ptr;
+	char arr[10]={'v','a','r','u','n'},*ptr;
 	ptr=arr;
 	cout<<"Printing array using index method"<<endl;
-	for(int i=0;arr[i]!='\0';i++)
+	for(int i=0;arr[i]!=NULL;i++)
 	cout<<arr[i];
 	cout<<"\nPrinting array using pointer method"<<endl;
-	for(int i=0;*(ptr+i)!='\0';i++)
+	for(int i=0;*(ptr+i)!=NULL;i++)
 	cout<<*(ptr+i);
 	return 0;
 }
